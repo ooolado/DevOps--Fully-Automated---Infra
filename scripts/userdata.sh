9267b272465b50c52e283d63a383c95aef6f6f10
Olanrewaju Oladoyin <olanrewaju_oladoyin@yahoo.com>
2024-03-13 22:07:58 -0500

    minor edits

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Hardware requirements: AWS Linux 2 with mimum t2.micro type instance & port 8080(application port), 9100 (node-exporter port) should be allowed on the security groups
-# setup for the ansible configuration
+# setup for the ansible configurations
 sudo yum update –y
 sudo useradd ansadmin
 sudo passwd ansadmin
